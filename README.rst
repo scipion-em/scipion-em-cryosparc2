649c2ba52ab2ce275a55a4517fb5a205417ef469
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-05-27 16:02:07 +0200

    Initial plugin version

---
+++ README.rst
@@ -0,0 +1,26 @@
+=================
+cryoSPARC2 plugin
+=================
+
+This plugin allows to use cryoSPARC2 programs within the Scipion framework
+
+
+Setup
+------
+
+- **Install this plugin:**
+
+.. code-block::
+
+    scipion installp -p scipion-em-cryosparc2
+
+OR
+
+  - through the plugin manager GUI by launching Scipion and following **Configuration** >> **Plugins**
+
+Alternatively, in devel mode:
+
+.. code-block::
+
+    scipion installp -p local/path/to/scipion-em-cryosparc2 --devel
+
---
+++ cryosparc2/bibtex.py
@@ -0,0 +1,63 @@
+# **************************************************************************
+# *
+# * Authors: Yunior C. Fonseca Reyna    (cfonseca@cnb.csic.es)
+# *
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+"""
+
+@article{Punjani2017,
+  Title                    = {cryoSPARC: algorithms for rapid unsupervised cryo-EM structure determination.},
+  Author                   = {Punjani, A., Rubinstein, J.L., Fleet, D.J. & Brubaker, M.A.},
+  Journal                  = {Nature Methods},
+  Year                     = {2017},
+  Month                    = {},
+  Number                   = {14},
+  Pages                    = {290-296},
+  Volume                   = {166},
+  Doi                      = {},
+  Keywords                 = {},
+  Pii                      = {},
+  Pmid                     = {},
+  Url                      = {https://www.nature.com/articles/nmeth.4169}
+}
+
+@article{Brubaker2017,
+  Title                    = {Building Proteins in a Day: Efficient 3D Molecular Reconstruction.},
+  Author                   = {Brubaker, M.A., Punjani, A & Fleet, D.J.},
+  Journal                  = {IEEE Trans. Pattern Anal. Mach. Intell.},
+  Year                     = {2017},
+  Month                    = {},
+  Number                   = {39},
+  Pages                    = {706-718},
+  Volume                   = {},
+  Doi                      = {},
+  Keywords                 = {},
+  Pii                      = {},
+  Pmid                     = {},
+  Url                      = {https://ieeexplore.ieee.org/document/7740877}
+}
+
+"""
+
