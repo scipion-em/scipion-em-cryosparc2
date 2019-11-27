4ede04a324a6bed975fefb3f7dca70e8980f20cb
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-11-27 08:53:10 +0100

    * Fixing minor error copying the cryosparc output

--- cryosparc2/protocols/protocol_cryosparc_part_subtract.py
+++ cryosparc2/protocols/protocol_cryosparc_part_subtract.py
@@ -204,7 +204,7 @@ class ProtCryoSparcSubtract(ProtOperateParticles):
 
         # Create the output folder
         os.system("cp -r " + self.projectPath + "/" + self.projectName.get() + '/' +
-            self.runPartStract.get() + "/*.*" + self._getExtraPath())
+            self.runPartStract.get() + "/*.*" + " " + self._getExtraPath())
 
         csFileName = "subtracted_particles.cs"
         csFile = os.path.join(self._getExtraPath(), csFileName)
