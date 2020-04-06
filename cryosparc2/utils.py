9a2734cdbc1b3a7b21115caab1c39570486ac772
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2020-04-06 09:12:55 +0200

    * Deleting redeclared method

--- cryosparc2/utils.py
+++ cryosparc2/utils.py
@@ -477,33 +477,6 @@ def addComputeSectionParams(form):
                   help='The scheduler lane name to add the protocol execution')
 
 
-def addComputeSectionParams(form):
-    """
-    Add the compute settings section
-    """
-    form.addParam('cacheParticlesSSD', BooleanParam, default=True,
-                  label='Cache particle images on SSD',
-                  help='Whether or not to copy particle images to the local '
-                       'SSD before running. The cache is persistent, so after '
-                       'caching once, particles should be available for '
-                       'subsequent jobs that require the same data. Not '
-                       'using an SSD can dramatically slow down processing.')
-
-    # Default behaviour to latest version
-    if (not isCryosparcRunning()) or (parse_version(getCryosparcInstalledVersion()) >= parse_version(V2_13_0)):
-        form.addHidden(GPU_LIST, StringParam, default='0',
-                       label='Choose GPU IDs:', validators=[NonEmpty],
-                       help='This argument is necessary. By default, the '
-                            'protocol will attempt to launch on GPU 0. You can '
-                            'override the default allocation by providing a '
-                            'list of which GPUs (0,1,2,3, etc) to use. '
-                            'GPU are separated by ",". For example: "0,1,5"')
-
-    form.addParam('compute_lane', StringParam, default='default',
-                  label='Lane name:',
-                  help='The scheduler lane name to add the protocol execution')
-
-
 def addSymmetryParam(form):
     """
     Add the symmetry param with the conventions
