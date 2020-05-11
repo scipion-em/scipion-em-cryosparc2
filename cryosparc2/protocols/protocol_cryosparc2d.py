106591958dfe80dfd058387cca428452bf775922
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2020-05-11 16:19:23 +0200

    * Fixing an error with the use of ssd

--- cryosparc2/protocols/protocol_cryosparc2d.py
+++ cryosparc2/protocols/protocol_cryosparc2d.py
@@ -532,7 +532,7 @@ class ProtCryo2D(ProtClassify2D):
                   "class2D_sigma_num_anneal_iters": str(self.iterationToStartAnneal.get()),
                   "class2D_sigma_use_white": str(self.useWhiteNoiseModel.get()),
                   "intermediate_plots": str('False'),
-                  "compute_use_ssd": str(self.cacheParticlesSSD.get()),
+                  "compute_use_ssd": str(self.compute_use_ssd.get()),
                   "compute_num_gpus": str(numberGPU)}
 
         self.runClass2D = enqueueJob(className, self.projectName.get(),
